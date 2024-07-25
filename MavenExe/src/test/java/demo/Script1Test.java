3d393eb701f2d28cba5ac41e934140ef762ab09b
scrap <scrap@TestMachine.home>
2024-07-25 15:39:57 +0200

    Dummy push

---
+++ MavenExe/src/test/java/demo/Script1Test.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Script1Test {
+
+	@Test
+	public void m1()
+	{
+		String Browser = System.getProperty("browser");
+		System.out.println(Browser);
+		String URL = System.getProperty("url");
+		System.out.println(URL);
+		//mvn -Dkey1=value1 -Dkey2= value2 test
+
+	}
+}
---
+++ MavenExe/src/test/java/demo/Script2Test.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Script2Test {
+
+	@Test
+	public void m2()
+	{
+		System.out.println("Bye");
+	}
+}
