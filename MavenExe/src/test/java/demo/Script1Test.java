3ec138996ceb75cbf24d4186d572639d5b48fa5a
scrap <scrap@TestMachine.home>
2024-07-25 16:24:28 +0200

    second push

--- MavenExe/src/test/java/demo/Script1Test.java
+++ MavenExe/src/test/java/demo/Script1Test.java
@@ -7,11 +7,12 @@ public class Script1Test {
 	@Test
 	public void m1()
 	{
-		String Browser = System.getProperty("browser");
+		/*String Browser = System.getProperty("browser");
 		System.out.println(Browser);
 		String URL = System.getProperty("url");
-		System.out.println(URL);
-		//mvn -Dkey1=value1 -Dkey2= value2 test
+		System.out.println(URL);*/
+		//mvn -Dkey1=value1 -Dkey2= value2 test*/
+		System.out.println("hi");
 
 	}
 }
