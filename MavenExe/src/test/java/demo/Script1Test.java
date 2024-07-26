6edd2f09097417ca70d242f08c27ed30777b308d
scrap <scrap@TestMachine.home>
2024-07-26 16:24:01 +0200

    uncomment

--- MavenExe/src/test/java/demo/Script1Test.java
+++ MavenExe/src/test/java/demo/Script1Test.java
@@ -7,8 +7,8 @@ public class Script1Test {
 	@Test
 	public void m1()
 	{
-		String Browser = System.getProperty("browser");
-		System.out.println(Browser);
+		/*String Browser = System.getProperty("browser");
+		System.out.println(Browser);*/
 		/*String URL = System.getProperty("url");
 		System.out.println(URL);*/
 		//mvn -Dkey1=value1 -Dkey2= value2 test*/
